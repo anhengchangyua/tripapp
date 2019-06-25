3ce5a0a871a30a203680e51cae6c624d455d63a6
timeflies <188851312@qq.com>
2019-06-25 17:58:11 +0800

    first commit

---
+++ lib/navigator/tab_navigator.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/page/MyPage.dart';
+import 'package:flutter_app/page/TravelPage.dart';
+import 'package:flutter_app/page/home_page.dart';
+import 'package:flutter_app/page/search_page.dart';
+
+class TabNavigator extends StatefulWidget {
+  @override
+  _TabNavigatorState createState() => _TabNavigatorState();
+}
+
+class _TabNavigatorState extends State<TabNavigator>
+    with SingleTickerProviderStateMixin {
+  var _controller = PageController(initialPage: 0);
+  final _defaultColor = Colors.grey;
+  final _activeColor = Colors.blue;
+  int _currentIndex = 0;
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('在东港'),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                print('ICON PRESS');
+              })
+        ],
+      ),
+      body: PageView(
+        controller: _controller,
+        children: <Widget>[HomePage(), SearchPage(), TravelPage(), MyPage()],
+        physics: NeverScrollableScrollPhysics(), //取消滑动
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        type: BottomNavigationBarType.fixed,
+        onTap: (index) {
+          _controller.jumpToPage(index);
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              color: _defaultColor,
+            ),
+            activeIcon: Icon(
+              Icons.home,
+              color: _activeColor,
+            ),
+            title: Text(
+              '首页',
+              style: TextStyle(
+                  color: _currentIndex != 0 ? _defaultColor : _activeColor),
+            ),
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.search,
+                color: _defaultColor,
+              ),
+              activeIcon: Icon(
+                Icons.search,
+                color: _activeColor,
+              ),
+              title: Text(
+                '搜索',
+                style: TextStyle(
+                    color: _currentIndex != 1 ? _defaultColor : _activeColor),
+              )),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.camera_alt,
+                color: _defaultColor,
+              ),
+              activeIcon: Icon(
+                Icons.camera_alt,
+                color: _activeColor,
+              ),
+              title: Text(
+                '旅拍',
+                style: TextStyle(
+                    color: _currentIndex != 2 ? _defaultColor : _activeColor),
+              )),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.account_circle,
+                color: _defaultColor,
+              ),
+              activeIcon: Icon(
+                Icons.account_circle,
+                color: _activeColor,
+              ),
+              title: Text(
+                '我的',
+                style: TextStyle(
+                    color: _currentIndex != 3 ? _defaultColor : _activeColor),
+              )),
+        ],
+      ),
+    );
+  }
+}
