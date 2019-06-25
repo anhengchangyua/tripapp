09434093b6b8abbd3cc3f0bbde0715a1ff6fc13c
timeflies <188851312@qq.com>
2019-06-25 21:09:49 +0800

    gradual change (渐变)

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -2,34 +2,91 @@ import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 
 // ignore: must_be_immutable
-class HomePage extends StatelessWidget {
+
+const APPBAR_SCROLL_OFFSET = 100;
+
+class HomePage extends StatefulWidget {
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   List _imageUrls = [
     'http://pages.ctrip.com/commerce/promote/20180718/yxzy/img/640sygd.jpg',
     'http://dimg04.c-ctrip.com/images/700u0r000000gxvb93E54_810_235_85.jpg',
     'http://dimg04.c-ctrip.com/images/700c10000000pdili7D8B_780_235_57.jpg',
   ];
 
+  double appBarAlpha = 0;
+
+  _onScroll(offset) {
+    print(offset);
+    double alpha = offset / APPBAR_SCROLL_OFFSET;
+    if (alpha < 0) {
+      alpha = 0;
+    } else if (alpha > 1) {
+      alpha = 1;
+    }
+    setState(() {
+      appBarAlpha = alpha;
+    });
+    print(appBarAlpha);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: <Widget>[
-          Container(
-            height: 160,
-            child: Swiper(
-              onTap: (int) {
-                print(int);
+    return Stack(
+      children: <Widget>[
+        Scaffold(
+            body: MediaQuery.removePadding(
+          removeTop: true,
+          context: context,
+          child: NotificationListener(
+              onNotification: (scrollNotification) {
+                if (scrollNotification is ScrollUpdateNotification &&
+                    scrollNotification.depth == 0) {
+                  _onScroll(scrollNotification.metrics.pixels);
+                }
               },
-              itemCount: _imageUrls.length,
-              autoplay: true,
-              itemBuilder: (BuildContext context, int index) {
-                return Image.network(_imageUrls[index], fit: BoxFit.fill);
-              },
-              pagination: SwiperPagination(),
+              child: ListView(
+                children: <Widget>[
+                  Container(
+                    height: 160,
+                    child: Swiper(
+                      onTap: (int) {
+                        print(int);
+                      },
+                      itemCount: _imageUrls.length,
+                      autoplay: true,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Image.network(_imageUrls[index],
+                            fit: BoxFit.fill);
+                      },
+                      pagination: SwiperPagination(),
+                    ),
+                  ),
+                  Container(
+                    height: 800,
+                    child: ListTile(
+                      title: Text('Test'),
+                    ),
+                  )
+                ],
+              )),
+        )),
+        Opacity(
+          child: Container(
+            height: 80,
+            decoration: BoxDecoration(color: Colors.white),
+            child: Center(
+              child: Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Text('首页'),
+              ),
             ),
-          )
-        ],
-      ),
+          ),
+          opacity: appBarAlpha,
+        )
+      ],
     );
   }
 }
